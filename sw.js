96406ebf1bd88113bed4d81b2a9b62e072c55b23
boucolivia-000 <bouc.olivia@gmail.com>
2026-08-21 12:26:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="nout-kaz-v4-sync";
+const CACHE="nout-kaz-v4-1";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./nout-kaz-icon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
