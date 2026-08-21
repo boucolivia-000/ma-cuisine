b77ff7adf82fa3a98b0b940664bde2c928f253b6
boucolivia-000 <bouc.olivia@gmail.com>
2026-08-21 18:09:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,42 @@
-const CACHE="nout-kaz-v4-3-3-info";
-const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./nout-kaz-icon.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="nout-kaz-v4-3-4-auto-update";
+const STATIC=["./manifest.json","./nout-kaz-icon.png"];
+
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).catch(()=>{}));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.mode==="navigate" || req.destination==="document"){
+    event.respondWith(
+      fetch(req,{cache:"no-store"})
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put("./index.html",copy)).catch(()=>{});
+          return response;
+        })
+        .catch(()=>caches.match("./index.html"))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(req)
+      .then(response=>{
+        if(req.method==="GET" && response.ok){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(req,copy)).catch(()=>{});
+        }
+        return response;
+      })
+      .catch(()=>caches.match(req))
+  );
+});
