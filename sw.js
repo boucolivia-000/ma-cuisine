aa3547caa9a8a2824c47830437cd776094ebf313
boucolivia-000 <bouc.olivia@gmail.com>
2026-08-21 12:39:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE="nout-kaz-v4-3";
+const CACHE="nout-kaz-v4-4";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./nout-kaz-icon.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("message",e=>{if(e.data==="SKIP_WAITING")self.skipWaiting()});
+self.addEventListener("fetch",e=>{
+ if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).catch(()=>caches.match("./index.html")));return}
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+});
