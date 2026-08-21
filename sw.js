a705fec632f0801988a366c19119f97fe1b0ed14
boucolivia-000 <bouc.olivia@gmail.com>
2026-08-21 11:23:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="ma-cuisine-v2-1";
-const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
+const CACHE="nout-kaz-v3";
+const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./nout-kaz-icon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
